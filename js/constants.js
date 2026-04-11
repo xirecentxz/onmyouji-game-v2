c5f9c55ad9496c19e001d7877ec213972d91f7e8
DwikySP <168646961+Dsetyop@users.noreply.github.com>
2026-04-11 08:47:35 +0700

    Update damage value for constant 10 in constants.js
    ganti total kata yang dibutuhkan dan damage untuk clear stage 10

--- js/constants.js
+++ js/constants.js
@@ -8,7 +8,7 @@ export const STAGE_CONFIG = {
     7: { target: 5,  dmg: 20 },
     8: { target: 10, dmg: 10 },
     9: { target: 15, dmg: 6.7 },
-    10: { target: 5,  dmg: 20 },
+    10: { target: 10,  dmg: 10 },
     11: { target: 25, dmg: 4 },
 };
 
